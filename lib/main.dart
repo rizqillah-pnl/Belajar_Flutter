8345d9fdd11e5256e37523aaf90e70ce03c8e0dc
FlutLab.io <>
2021-10-11 05:42:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Coba-coba'),F
-            ),
-            body: Center(
-              child: Text(
-                'Hello World',
-                style: TextStyle(fontSize: 40),
-                textAlign: TextAlign.center,
-              ),
-            )
-        )
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Coba-coba'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(fontSize: 40),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
