8b4865ab351676a168d522f7898197de3b3b3cb3
FlutLab.io <>
2021-10-14 05:17:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,16 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Colors.red,
         ),
         body: Center(
-          child: AnimatedContainer(
-            color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
-            duration: Duration(seconds: 1),
-            width: 50.0 + random.nextInt(101),
-            height: 50.0 + random.nextInt(101),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
+              duration: Duration(seconds: 1),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),
+            ),
           ),
         ),
       ),
