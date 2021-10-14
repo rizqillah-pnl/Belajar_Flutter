37a96a3970e251c03218f2c5107969b5ac326611
FlutLab.io <>
2021-10-14 05:12:26 +0000

    Autosave

---
+++ lib/main7.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("RIZQILLAH / 1957301020"),
+        ),
+        body: Center(
+          child: Text(
+            "Belajar Text Style.",
+            style: TextStyle(fontFamily: "CrashLandingBB", fontSize: 45, decoration: TextDecoration.overline, decorationColor: Colors.red, decorationThickness: 4, decorationStyle: TextDecorationStyle.dashed),
+          ),
+        ),
+      ),
+    );
+  }
+}
