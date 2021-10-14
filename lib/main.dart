3f50511179976ddaf279edad43a0aed7770096b3
FlutLab.io <>
2021-10-14 05:29:24 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,19 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("RIZQILLAH / 1957301020"),
-          backgroundColor: Colors.red,
         ),
         body: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {});
-            },
-            child: AnimatedContainer(
-              color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
-              duration: Duration(seconds: 1),
-              width: 50.0 + random.nextInt(101),
-              height: 50.0 + random.nextInt(101),
-            ),
+          child: Text(
+            "Belajar Text Style.",
+            style: TextStyle(fontFamily: "CrashLandingBB", fontSize: 45, decoration: TextDecoration.overline, decorationColor: Colors.red, decorationThickness: 4, decorationStyle: TextDecorationStyle.dashed),
           ),
         ),
       ),
