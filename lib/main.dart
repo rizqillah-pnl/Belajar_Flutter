7c88296cf26bccd4fbe63436ae14ce79c26fe1c6
FlutLab.io <>
2021-10-14 04:42:23 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,46 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int angka = 0;
-
-  void pushButton() {
-    setState(() {
-      angka += 1;
-    });
-  }
-
-  void reset() {
-    setState(() {
-      angka = 0;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("RIZQILLAH / 1957301020")),
+        appBar: AppBar(
+          title: Text("RIZQILLAH / 1957301020"),
+        ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(angka.toString(), style: TextStyle(fontSize: 10 + angka.toDouble())),
-              RaisedButton(
-                child: Text("Tambah Angka"),
-                onPressed: pushButton,
-              ),
-              RaisedButton(
-                child: Text("Reset"),
-                onPressed: reset,
-              )
-            ],
+          child: Text(
+            "Belajar Text Style.",
+            style: TextStyle(fontFamily: "CrashLandingBB"),
           ),
         ),
       ),
