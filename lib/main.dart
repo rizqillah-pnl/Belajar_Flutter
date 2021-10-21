736d3fc1f98c196912e788d2b743a0573c376e79
FlutLab.io <>
2021-10-21 05:45:46 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,33 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.green,
         ),
         body: Center(
-          child: Container(
-            color: Colors.black,
-            width: 200,
-            height: 200,
-            padding: EdgeInsets.all(3),
-            child: Image(
-              image: NetworkImage(
-                "https://avatars.githubusercontent.com/u/71523218?v=4",
+          child: Column(
+            children: <Widget>[
+              Container(
+                color: Colors.black,
+                width: 200,
+                height: 200,
+                padding: EdgeInsets.all(3),
+                child: Image(
+                  image: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/71523218?v=4",
+                  ),
+                  fit: BoxFit.contain,
+                ),
               ),
-              fit: BoxFit.contain,
-            ),
+              Container(
+                color: Colors.black,
+                width: 200,
+                height: 200,
+                padding: EdgeInsets.all(3),
+                child: Image(
+                  image: AssetImage(
+                    "assets/image/logo.png",
+                  ),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ],
           ),
         ),
       ),
