4a5a5981174fa2696a4c82ea57bf983637e996bc
FlutLab.io <>
2021-10-22 02:43:40 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.green,
-        appBar: AppBar(
-          title: Text('RIZQILLAH / 1957301020 - SECOND'),
-          backgroundColor: Colors.red,
-        ),
         body: Container(
           margin: EdgeInsets.all(10),
           child: ListView(),
