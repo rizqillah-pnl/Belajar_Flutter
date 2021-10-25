6f390eceef715f6d4f1db344c446e589fb1bdc5e
FlutLab.io <>
2021-10-25 04:03:42 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,102 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-            title: Text('GeeksforGeeks'),
-            backgroundColor: Colors.greenAccent[400],
-            leading: IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {},
-              tooltip: 'Menu',
-            ) //IconButton
-            ), //AppBar
-        body: Center(
-          child: Container(
-              child: Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      width: 175,
-                      height: 175,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.red,
-                      ), //BoxDecoration
-                    ), //Container
-                    SizedBox(
-                      width: 20,
-                    ), //SizedBox
-                    Container(
-                        width: 175,
-                        height: 175,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.red,
-                        ) //BoxDecoration
-                        ) //Container
-                  ], //<Widget>[]
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ), //Row
-                Container(
-                  width: 380,
-                  height: 200,
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.blue), //BoxDecoration
-                ), //Container
-                Row(
-                  children: <Widget>[
-                    Container(
-                      width: 180,
-                      height: 300,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.cyan,
-                      ), //BoxDecoration
-                    ), //Container
-                    SizedBox(
-                      width: 20,
-                    ), //SizedBox
-                    Container(
-                        width: 180,
-                        height: 300,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.cyan,
-                        ) //BoxedDecoration
-                        ) //Container
-                  ], //<Widget>[]
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ), //Row
-              ], //<widget>[]
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-            ), //Column
-          ) //Padding
-              ), //Container
-        ) //Center
-        ), //Scaffold
+void main() => runApp(MyApp());
 
-    debugShowCheckedModeBanner: false,
-  )); //MaterialApp
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("RIZQILLAH / 1957301020"),
+          backgroundColor: Colors.red,
+        ),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.pink,
+                      foregroundDecoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://i.ytimg.com/vi/5cjWMAAbJPI/hqdefault.jpg",
+                            ),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.pink[100],
+                      foregroundDecoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://i.ytimg.com/vi/QZAc1hT1vdg/hqdefault.jpg",
+                            ),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.pink[300],
+                      foregroundDecoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/71523218?v=4",
+                            ),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.amber[200],
+                  borderRadius: BorderRadius.circular(40),
+                  image: DecorationImage(image: AssetImage('assets/images/logo.png'), fit: BoxFit.fill),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.blueGrey,
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                      child: Container(
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    Spacer(
+                      flex: 2,
+                    ),
+                    Flexible(
+                      child: Container(
+                        color: Colors.red[300],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
