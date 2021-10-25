ba2e1ab8cc9209521bb323214693540640cd3844
FlutLab.io <>
2021-10-25 03:59:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,85 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
         appBar: AppBar(
-          title: Text("RIZQILLAH / 1957301020 - Flexible Layout"),
-          backgroundColor: Colors.green,
-        ),
-        body: Column(
-          children: <Widget>[
-            Flexible(
-              flex: 1,
-              child: Row(
-                children: <Widget>[
-                  Flexible(
-                    flex: 2,
-                    child: Container(
-                      margin: EdgeInsets.all(5),
-                      color: Colors.red,
-                    ),
-                  ),
-                  Flexible(
-                    flex: 2,
-                    child: Container(
-                      margin: EdgeInsets.all(5),
-                      color: Colors.green,
-                    ),
-                  ),
-                  Flexible(
-                    flex: 2,
-                    child: Container(
-                      margin: EdgeInsets.all(5),
-                      color: Colors.purple,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                margin: EdgeInsets.all(5),
-                color: Colors.amber,
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                margin: EdgeInsets.all(5),
-                color: Colors.blue,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+            title: Text('GeeksforGeeks'),
+            backgroundColor: Colors.greenAccent[400],
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+              tooltip: 'Menu',
+            ) //IconButton
+            ), //AppBar
+        body: Center(
+          child: Container(
+              child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 175,
+                      height: 175,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.red,
+                      ), //BoxDecoration
+                    ), //Container
+                    SizedBox(
+                      width: 20,
+                    ), //SizedBox
+                    Container(
+                        width: 175,
+                        height: 175,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.red,
+                        ) //BoxDecoration
+                        ) //Container
+                  ], //<Widget>[]
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ), //Row
+                Container(
+                  width: 380,
+                  height: 200,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.blue), //BoxDecoration
+                ), //Container
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 180,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.cyan,
+                      ), //BoxDecoration
+                    ), //Container
+                    SizedBox(
+                      width: 20,
+                    ), //SizedBox
+                    Container(
+                        width: 180,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.cyan,
+                        ) //BoxedDecoration
+                        ) //Container
+                  ], //<Widget>[]
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ), //Row
+              ], //<widget>[]
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+            ), //Column
+          ) //Padding
+              ), //Container
+        ) //Center
+        ), //Scaffold
+
+    debugShowCheckedModeBanner: false,
+  )); //MaterialApp
 }
---
+++ lib/main12-flex.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("RIZQILLAH / 1957301020 - Flexible Layout"),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.red,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.green,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.purple,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                color: Colors.amber,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
