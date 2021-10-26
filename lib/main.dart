cdb5acb0c15284d0c550490eca391f9c8b91483c
FlutLab.io <>
2021-10-26 08:24:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,99 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("RIZQILLAH / 1957301020 - TextField"),
-          backgroundColor: Colors.blue,
+          title: Text("RIZQILLAH / 1957301020"),
+          backgroundColor: Colors.red,
         ),
-        body: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              TextField(
-                maxLength: 10,
-                obscureText: false,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                controller: controller,
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.pink,
+                      foregroundDecoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://i.ytimg.com/vi/5cjWMAAbJPI/hqdefault.jpg",
+                            ),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.pink[100],
+                      foregroundDecoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://i.ytimg.com/vi/QZAc1hT1vdg/hqdefault.jpg",
+                            ),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.pink[300],
+                      foregroundDecoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/71523218?v=4",
+                            ),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Text(controller.text)
-            ],
-          ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.amber[200],
+                  borderRadius: BorderRadius.circular(40),
+                  image: DecorationImage(image: AssetImage('assets/images/logo.png'), fit: BoxFit.fill),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.blueGrey,
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                      child: Container(
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    Spacer(
+                      flex: 2,
+                    ),
+                    Flexible(
+                      child: Container(
+                        color: Colors.red[300],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
