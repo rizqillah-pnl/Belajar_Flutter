9931398c5da54a5c6fb64bcd541bc1b93f3ed5dd
FlutLab.io <>
2021-10-28 06:28:49 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/services.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
+    DeviceOrientation.portraitUp
   ]);
   runApp(new MyApp());
 }
