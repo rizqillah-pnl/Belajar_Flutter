b805d2907766d6650deb0a7de58d2d25c4ebd6e7
FlutLab.io <>
2021-10-28 04:18:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
   double getSmallDiameter(BuildContext context) => MediaQuery.of(context).size.width * 2 / 3;
   double getBigDiameter(BuildContext context) => MediaQuery.of(context).size.width * 7 / 8;
 
@@ -11,10 +21,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("RIZQILLAH / 1957301020 - Image"),
-          backgroundColor: Colors.green,
-        ),
         body: Stack(
           children: <Widget>[
             Container(
--- lib/main20-login.dart
+++ lib/main20-login.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  double getSmallDiameter(BuildContext context) => MediaQuery.of(context).size.width * 2 / 3;
+  double getBigDiameter(BuildContext context) => MediaQuery.of(context).size.width * 7 / 8;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              width: getSmallDiameter(context),
+              height: getSmallDiameter(context),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(colors: [
+                    Color(0xFF022602),
+                    Color(0xFFF6DA7)
+                  ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
