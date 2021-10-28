336cf78cf661b9dc7152515a6c56723ecf373dc6
FlutLab.io <>
2021-10-28 04:05:50 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  double getSmallDiameter(BuildContext context) => MediaQuery.of(context).size.width = 2 / 3;
+  double getBigDiameter(BuildContext) => MediaQuery.of(context).size.width = 7 / 8;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,21 +15,20 @@ class MyApp extends StatelessWidget {
           title: Text("RIZQILLAH / 1957301020 - Image"),
           backgroundColor: Colors.green,
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                color: Colors.black,
-                width: 200,
-                height: 200,
-                padding: EdgeInsets.all(3),
-                child: Image(
-                  image: AssetImage('assets/images/logo.png'),
-                  fit: BoxFit.contain,
-                ),
+        body: Stack(
+          children: <Widget>[Container(
+            width: getSmallDiameter(context),
+            height: getSmallDiameter(context),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: [Color(0xFF022602),Color(0xFFF6DA7)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter)),
               ),
-            ],
+            ),
           ),
+          ],
         ),
       ),
     );
