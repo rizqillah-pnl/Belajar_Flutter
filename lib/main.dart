66f2648ec80f6b886ba4535e9f027cb6dffc6491
FlutLab.io <>
2021-11-11 05:10:39 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -21,86 +21,55 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  final List<String> bahasa = [
-    'Java',
-    'Kotlin',
-    'Dart'
-  ];
-  bool selected1 = false;
-  bool selected2 = false;
-  bool selected3 = false;
-  List<int> list = [];
+  int selected = -1;
 
-  void onChanged1(bool value) {
+  void onChanged(int value) {
     setState(() {
-      this.selected1 = value;
+      this.selected = value;
     });
-    if (value)
-      list.add(0);
-    else
-      list.remove(0);
-    print(list);
-  }
-
-  void onChanged2(bool value) {
-    setState(() {
-      this.selected2 = value;
-    });
-    if (value)
-      list.add(1);
-    else
-      list.remove(1);
-    print(list);
-  }
-
-  void onChanged3(bool value) {
-    setState(() {
-      this.selected3 = value;
-    });
-    if (value)
-      list.add(2);
-    else
-      list.remove(2);
-    print(list);
+    print('Pilihan : ${this.selected}');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo Checkbox'),
+        title: Text('Demo Radio'),
       ),
       body: Container(
         padding: EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
-            Text('Bahasa yang disukai : '),
-            CheckboxListTile(
-              value: this.selected1,
-              onChanged: (value) {
-                onChanged1(value);
-              },
-              title: Text(this.bahasa[0]),
-              activeColor: Colors.red,
-              secondary: Icon(Icons.language),
+            Text('Jenis Kelamin: '),
+            Row(
+              children: <Widget>[
+                Radio(
+                  value: 88,
+                  groupValue: this.selected,
+                  onChanged: (value) {
+                    onChanged(value);
+                  },
+                ),
+                Container(
+                  width: 8.0,
+                ),
+                Text('Pria'),
+              ],
             ),
-            CheckboxListTile(
-              value: this.selected2,
-              onChanged: (value) {
-                onChanged2(value);
-              },
-              title: Text(this.bahasa[1]),
-              activeColor: Colors.red,
-              secondary: Icon(Icons.language),
-            ),
-            CheckboxListTile(
-              value: this.selected3,
-              onChanged: (value) {
-                onChanged3(value);
-              },
-              title: Text(this.bahasa[2]),
-              activeColor: Colors.red,
-              secondary: Icon(Icons.language),
+            Row(
+              children: <Widget>[
+                Radio(
+                  value: 99,
+                  groupValue: this.selected,
+                  onChanged: (value) {
+                    onChanged(value);
+                  },
+                ),
+                Container(
+                  width: 8.0,
+                ),
+                Text('Wanita'),
+              ],
             ),
           ],
         ),
