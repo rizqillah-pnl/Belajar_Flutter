604f1aa63c304caa826f0e9679509ff32c128a7b
FlutLab.io <>
2021-11-24 13:36:59 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -21,46 +21,117 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  int selected = -1;
+  final List<String> settings = [
+    'Airplane Mode',
+    'WLAN',
+    'Mobile Data',
+    'Bluetooth'
+  ];
+  List<bool> settingValues = [
+    false,
+    false,
+    false,
+    false
+  ];
 
-  void onChanged(int value) {
+  void onSwitch1Changed(bool value) {
     setState(() {
-      this.selected = value;
+      this.settingValues[0] = value;
     });
-    print('Pilihan : ${this.selected}');
+    print(settingValues);
+  }
+
+  void onSwitch2Changed(bool value) {
+    setState(() {
+      this.settingValues[1] = value;
+    });
+    print(settingValues);
+  }
+
+  void onSwitch3Changed(bool value) {
+    setState(() {
+      this.settingValues[2] = value;
+    });
+    print(settingValues);
+  }
+
+  void onSwitch4Changed(bool value) {
+    setState(() {
+      this.settingValues[3] = value;
+    });
+    print(settingValues);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo Radio'),
+        title: Text('Demo Switch'),
       ),
       body: Container(
         padding: EdgeInsets.all(10),
-        child: Column(
+        child: ListView(
           children: <Widget>[
-            Text('Jenis Kelamin: '),
-            RadioListTile(
-              value: 0,
-              groupValue: this.selected,
-              onChanged: (value) {
-                onChanged(value);
-              },
-              title: Text('Pria'),
-              activeColor: Colors.red,
-              secondary: Icon(Icons.group),
+            Row(
+              children: <Widget>[
+                Switch(
+                  value: this.settingValues[0],
+                  onChanged: (value) {
+                    onSwitch1Changed(value);
+                  },
+                ),
+                Container(
+                  width: 8.0,
+                ),
+                Text(this.settings[0]),
+              ],
             ),
-            RadioListTile(
-              value: 1,
-              groupValue: this.selected,
-              onChanged: (value) {
-                onChanged(value);
-              },
-              title: Text('Wanita'),
-              activeColor: Colors.red,
-              secondary: Icon(Icons.group),
+            Divider(),
+            Row(
+              children: <Widget>[
+                Switch(
+                  value: this.settingValues[1],
+                  onChanged: (value) {
+                    onSwitch2Changed(value);
+                  },
+                ),
+                Container(
+                  width: 8.0,
+                ),
+                Text(this.settings[1]),
+              ],
             ),
+            Divider(),
+            Row(
+              children: <Widget>[
+                Switch(
+                  value: this.settingValues[2],
+                  onChanged: (value) {
+                    onSwitch3Changed(value);
+                  },
+                ),
+                Container(
+                  width: 8.0,
+                ),
+                Text(this.settings[2]),
+              ],
+            ),
+            Divider(),
+            Row(
+              children: <Widget>[
+                Switch(
+                  value: this.settingValues[3],
+                  onChanged: (value) {
+                    onSwitch4Changed(value);
+                  },
+                ),
+                Container(
+                  width: 8.0,
+                ),
+                Text(this.settings[3]),
+              ],
+            ),
+            Divider(),
           ],
         ),
       ),
