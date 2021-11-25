dfd942507e9edbab20b845626c1894d1706ef18a
FlutLab.io <>
2021-11-25 03:23:20 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -21,91 +21,32 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  final List<String> settings = [
-    'Airplane Mode',
-    'WLAN',
-    'Mobile Data',
-    'Bluetooth'
-  ];
-  List<bool> settingValues = [
-    false,
-    false,
-    false,
-    false
-  ];
+  double sliderValue = 0.0;
 
-  void onSwitch1Changed(bool value) {
+  void onChanged(double value) {
     setState(() {
-      this.settingValues[0] = value;
+      this.sliderValue = value;
     });
-    print(settingValues);
-  }
-
-  void onSwitch2Changed(bool value) {
-    setState(() {
-      this.settingValues[1] = value;
-    });
-    print(settingValues);
-  }
-
-  void onSwitch3Changed(bool value) {
-    setState(() {
-      this.settingValues[2] = value;
-    });
-    print(settingValues);
-  }
-
-  void onSwitch4Changed(bool value) {
-    setState(() {
-      this.settingValues[3] = value;
-    });
-    print(settingValues);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo Switch'),
+        title: Text('Demo Slider'),
       ),
       body: Container(
         padding: EdgeInsets.all(10),
-        child: ListView(
+        child: Column(
           children: <Widget>[
-            SwitchListTile(
-              value: this.settingValues[0],
+            Text('Nilai Aktif : ${sliderValue.round()}'),
+            Slider(
+              min: 0.0,
+              max: 100.0,
+              value: this.sliderValue,
               onChanged: (value) {
-                onSwitch1Changed(value);
+                onChanged(value);
               },
-              title: Text(this.settings[0]),
-              secondary: Icon(Icons.airplanemode_active),
-            ),
-            Divider(),
-            SwitchListTile(
-              value: this.settingValues[1],
-              onChanged: (value) {
-                onSwitch2Changed(value);
-              },
-              title: Text(this.settings[1]),
-              secondary: Icon(Icons.wifi),
-            ),
-            Divider(),
-            SwitchListTile(
-              value: this.settingValues[2],
-              onChanged: (value) {
-                onSwitch3Changed(value);
-              },
-              title: Text(this.settings[2]),
-              secondary: Icon(Icons.data_usage),
-            ),
-            Divider(),
-            SwitchListTile(
-              value: this.settingValues[3],
-              onChanged: (value) {
-                onSwitch4Changed(value);
-              },
-              title: Text(this.settings[3]),
-              secondary: Icon(Icons.bluetooth),
             ),
           ],
         ),
