0eef0aad934bcea8b1869019cfe1daf0b886eae2
FlutLab.io <>
2021-12-02 05:15:32 +0000

    Autosave

---
+++ lib/login.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/constants.dart';
+import 'package:helloworld/register.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: ColorPalette.primaryColor,
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  _iconLogin(),
+                  _titleDescription(),
+                  _textField(),
+                  _buildButton(context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _iconLogin() {
+    return Icon(
+      Icons.people,
+      color: Colors.white,
+      size: 150.0,
+    );
+  }
+
+  Widget _titleDescription() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        Text(
+          "Login Into App",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        Text(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio felis, gravida id congue nec, imperdiet quis diam. Sed auctor efficitur cursus. Sed blandit porta elit a cursus. Etiam nunc nibh, auctor ut justo et, convallis mattis dui.",
+          style: TextStyle(
+            fontSize: 12.0,
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget _textField() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Username",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: TextStyle(color: Colors.white),
+          autofocus: false,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Password",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: TextStyle(color: Colors.white),
+          obscureText: true,
+          autofocus: false,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        InkWell(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            width: double.infinity,
+            child: Text(
+              "Login",
+              style: TextStyle(color: ColorPalette.primaryColor),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        Text(
+          "or",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+        FlatButton(
+            child: Text(
+              "Register",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, RegisterPage.routeName);
+            }),
+      ],
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/constants.dart';
+
+class RegisterPage extends StatelessWidget {
+  static const routeName = "/registerPage";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: ColorPalette.primaryColor,
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  _iconRegister(),
+                  _titleDescription(),
+                  _textField(),
+                  _buildButton(context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _iconRegister() {
+    return Icon(
+      Icons.people,
+      color: Colors.white,
+      size: 150.0,
+    );
+  }
+
+  Widget _titleDescription() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        Text(
+          "Registration",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        Text(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio felis, gravida id congue nec, imperdiet quis diam. Sed auctor efficitur cursus. Sed blandit porta elit a cursus. Etiam nunc nibh, auctor ut justo et, convallis mattis dui.",
+          style: TextStyle(
+            fontSize: 12.0,
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget _textField() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Username",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: TextStyle(color: Colors.white),
+          autofocus: false,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Password",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: TextStyle(color: Colors.white),
+          obscureText: true,
+          autofocus: false,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Confirm Password",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: TextStyle(color: Colors.white),
+          obscureText: true,
+          autofocus: false,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        InkWell(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            width: double.infinity,
+            child: Text(
+              "Register",
+              style: TextStyle(color: ColorPalette.primaryColor),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        Text(
+          "or",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+        FlatButton(
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, "/");
+          },
+        ),
+      ],
+    );
+  }
+}
