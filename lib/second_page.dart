765bd340cdaccd91c009ccf9fd48bc2114a98f3d
FlutLab.io <>
2021-12-08 04:43:52 +0000

    Autosave

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:helloworld/main_page.dart';
+// import 'package:flutter/material.dart';
+// import 'package:helloworld/main_page.dart';
 
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('RIZQILLAH / 1957301020 - SECOND'),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: ElevatedButton(
-            child: Text('Back'),
-            onPressed: () {
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-                return MainPage();
-              }));
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class SecondPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('RIZQILLAH / 1957301020 - SECOND'),
+//           backgroundColor: Colors.red,
+//         ),
+//         body: Center(
+//           child: ElevatedButton(
+//             child: Text('Back'),
+//             onPressed: () {
+//               Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+//                 return MainPage();
+//               }));
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
